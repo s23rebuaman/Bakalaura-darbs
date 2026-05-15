a55cf4e191bdee7850ecd9ce45fe796b5b3d552c
s23rebuaman <s23rebuaman@venta.lv>
2026-05-15 19:48:50 +0300

    WIP

--- main.py
+++ main.py
@@ -24,11 +24,52 @@
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('data_filter.html')
+
+# @app.route('/data_result', methods=['POST'])
+# def data_result():
+    # # if request.method == 'POST':
+    # #     return render_template('data_result.php')
+    # year = request.form.get('years')
+    # month = request.form.get('months')
+    # day = request.form.get('days')
+    # date = request.form.get('dates')
+
+    # return render_template('data_result.html', year=year, month=month, day=day, date=date)
+
+db_credentials = {
+    'user': 'root',  
+    'password': 'rootAdmin.2004', 
+    'host': '127.0.0.1',
+    'name': 'ventspils_reiss'  
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://\
+{db_credentials['user']}:{db_credentials['password']}@{db_credentials['host']}/\
+{db_credentials['name']}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# INITIALIZE DATABASE AFTER CONFIGURATION
+db = SQLAlchemy(app)
+
+# APP ROUTE TO GET RESULTS FOR SELECT QUERY
+@app.route('/query_result', methods=['POST'])
+def get_results():
+    year = request.form.get('years')
+    month = request.form.get('months')
+    day = request.form.get('days')
+    date = request.form.get('dates')
+    print(year, month, day, date)
+
+    result = db.engine.execute('select Marsruta_nr, Reisa_nosaukums, Datums_un_laiks, count(Bilesu_sk) from info where year(Datums_un_laiks) = 2023 and month(Datums_un_laiks) = 2 and day(Datums_un_laiks) = 2 and Bilesu_sk=(select max(Bilesu_sk))')
+    response = {f'Record {i}': list(each) for i, each in enumerate(result, start=1)}
+    return response
+
 
 app.run()
--- mysql_ventspils-reiss.sql
+++ mysql_ventspils-reiss.sql
@@ -48,11 +48,55 @@ and day(Datums_un_laiks) = 2
 and Bilesu_sk=(select max(Bilesu_sk))
 */
 
--- noslogotākais maršruts 2025.gada 29.decembrī pulkstens 22:10
-select Marsruta_nr, Reisa_nosaukums, Datums_un_laiks, count(Bilesu_sk)
+-- noslogotākais maršruts 2025.gada 29.decembrī pulkstens 18:55
+/*
+select Marsruta_nr, Reisa_nosaukums, Datums_un_laiks, max(Bilesu_sk)
 from info
 where year(Datums_un_laiks) = 2025
 and month(Datums_un_laiks) = 12
 and day(Datums_un_laiks) = 29
-and time(Datums_un_laiks) = 2210
-and Bilesu_sk=(select max(Bilesu_sk))
+and time(Datums_un_laiks) = 185500
+*/
+
+-- maršruti 2024.gada 13.augustā no pulkstens 10:00 līdz 12:00
+/*
+select Marsruta_nr, Reisa_nosaukums, Datums_un_laiks, Bilesu_sk
+from info
+where year(Datums_un_laiks) = 2024
+and month(Datums_un_laiks) = 8
+and day(Datums_un_laiks) = 13
+and (time(Datums_un_laiks) > 100000 and time(Datums_un_laiks) < 120000)
+order by Bilesu_sk desc
+*/
+
+-- noslogotākais maršruts 2024.gada 13.augustā no pulkstens 10:00 līdz 12:00
+/*
+select Marsruta_nr, Reisa_nosaukums, Datums_un_laiks, max(Bilesu_sk)
+from info
+where year(Datums_un_laiks) = 2024
+and month(Datums_un_laiks) = 8
+and day(Datums_un_laiks) = 13
+and (time(Datums_un_laiks) > 100000 and time(Datums_un_laiks) < 120000)
+*/
+
+-- distinct laiki (718 - 2023, 777 - 2024, 767 - 2025)
+/*
+select distinct time(Datums_un_laiks) from ventspils_reiss_dati
+where year(Datums_un_laiks) = 2025
+*/
+
+-- select latest time value
+-- select max(time(Datums_un_laiks)) from ventspils_reiss_dati
+
+-- select earliest time value
+-- select min(time(Datums_un_laiks)) from ventspils_reiss_dati
+
+-- select time from 22:00 till 06:00
+/*
+select distinct time(Datums_un_laiks) from ventspils_reiss_dati
+-- WHERE (time(Datums_un_laiks) >= '22:00:00' 
+-- AND time(Datums_un_laiks) <= '23:59:59')
+where (time(Datums_un_laiks) >= '00:00:00' 
+AND time(Datums_un_laiks) <= '05:59:59')
+order by time(Datums_un_laiks) desc
+*/
